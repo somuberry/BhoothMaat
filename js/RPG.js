bbf65964a23e6b914466fa8ec91668af4c167ab9
somuberry <saumyaoberoi20@gmail.com>
2022-03-30 10:18:39 +0530

    Update RPG.js

--- js/RPG.js
+++ js/RPG.js
@@ -205,5 +205,15 @@ $("#samaybeeta").click(function(){
 	
 });
 
+// loader
+var myVar;
 
+function myFunction() {
+  myVar = setTimeout(showPage, 2000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("container").style.display = "block";
+}
 
